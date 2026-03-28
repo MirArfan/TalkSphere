69739150556f164da7a4b1934587934084513197
Mir Arfan Uddin <mir.arfan.iiuc9@gmail.com>
2026-03-28 12:31:41 +0600

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+
+
+dotenv.config();
+
+const app=express();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("server is run+ing 3000 00");
+})
