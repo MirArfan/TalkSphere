81dfa40aac6c016430a22168eb52cd150891bf7b
Mir Arfan Uddin <mir.arfan.iiuc9@gmail.com>
2026-05-09 15:59:27 +0600

    login and logout implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 function ChatPage() {
+  const {logout} = useAuthStore();
+
   return (
-    <div>ChatPage</div>
+    <div className='z-10'>
+      ChatPage
+      <button className='text-red-600' onClick={logout}>Logout</button>
+    </div>
   )
 }
 
