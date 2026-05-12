307a664dd10ef74c199cedc1adb4f9252d008005
Mir Arfan Uddin <mir.arfan.iiuc9@gmail.com>
2026-05-12 12:19:18 +0600

    sidebar completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -57,6 +57,16 @@ export const useAuthStore = create((set, get) => ({
         } catch (error) {
             toast.error("Logout error : ", error);
         }
+    },
+    updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.data.message);
     }
+  },
 
 }))
