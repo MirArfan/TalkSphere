811cc52873fe9825625eec7c58a83274d6048574
Mir Arfan Uddin <mir.arfan.iiuc9@gmail.com>
2026-05-13 11:44:37 +0600

    send message functionality implemented

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,11 +1,12 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { useAuthStore } from "./useAuthStore";
 
 export const useChatStore = create ((set, get)=>({
     allContacts:[],
     chats:[],
-    message:[],
+    messages:[],
     activeTab:"chats",
     selectedUser:null,
     isUsersLoading: false,
@@ -49,11 +50,39 @@ export const useChatStore = create ((set, get)=>({
         set({isMessagesLoading: true});
         try {
             const res= await axiosInstance.get(`/message/${userId}`);
-            set({message: res.data});
+            set({messages: res.data});
         } catch (error) {
              toast.error(error.response?.data?.message || "Someting went wrong");
         } finally{
             set({isMessagesLoading: false});
+        }
+    },
+    sendMessage: async(messageData)=>{
+        const {selectedUser, messages}= get();
+
+        const { authUser } = useAuthStore.getState();
+
+        const tempId = `temp-${Date.now()}`;
+
+        const optimisticMessage = {
+            _id: tempId,
+            senderId: authUser._id,
+            receiverId: selectedUser._id,
+            text: messageData.text,
+            image: messageData.image,
+            createdAt: new Date().toISOString(),
+            isOptimistic: true, // flag to identify optimistic messages (optional)
+        };
+
+        // immidetaly update the ui by adding the message
+        set({ messages: [...messages, optimisticMessage] });
+    
+        try {
+            const res= await axiosInstance.post(`/message/send/${selectedUser._id}`, messageData);
+            set({messages: messages.concat(res.data)});
+        } catch (error) {
+            set({messages: messages}) //for failure message to remove
+            toast.error(error.response?.data?.message || "Someting went wrong");
         }
     }
 }))
